356c67dd2b93385997ea1829ed98b0f8583bb6b1
Rajat Singh <therevolutionizt@gmail.com>
2015-03-23 08:58:40 +0530

    getting function names with some extra non essential info

--- getfunctions.py
+++ getfunctions.py
@@ -2,14 +2,16 @@
 
 import sys, getopt, os, re
 
+not_required = ['if', 'else', 'while']
+
 def main(argv):
     inputfile = ''
     try:
         opts, args = getopt.getopt(argv,"i:",["ifile="])
     except getopt.GetoptError:
         usage()
-    if len(opts)<2:
-        usage()
+    #if len(opts)<2:
+        #usage()
     for opt, arg in opts:
         if opt == '-h':
             usage()
@@ -20,6 +22,20 @@ def main(argv):
     # abs_in is the absolute path of input file
     abs_in = os.path.realpath(os.path.join(inputfile))
     print abs_in
+    parser(abs_in)
+
+def parser(inp):
+    openfile = open(inp,'r')
+    methods=[] 
+    for line in openfile:
+        if '(' in line:
+            tokens=line.split('(')
+            token=tokens[0].split()
+            if(len(token)>0):
+                methods.append(token[-1])
+    methods=list(set(methods)-set(not_required)) #O(N)
+    for x in methods:
+        print x
 
 def usage():
     print './getfunctions.py -i <inputfile> '
